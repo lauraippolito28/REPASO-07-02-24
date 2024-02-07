a40d2cca7ead1e03e1db705a22d22e4275d1adf0
lauraippolito28 <159057618+lauraippolito28@users.noreply.github.com>
2024-02-06 22:07:03 -0500

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    //Variables 
+    string usuario = "laura.ippolito";
+    string contrasena = "Laura.123";
+    string input_usuario, input_contrasena;
+    
+    // Entradas
+    cout << "Ingresa tu usuario: ";
+    cin >> input_usuario;
+    cout << "Ingresa tu contraseña: ";
+    cin >> input_contrasena;
+    
+    //Proceso y salida 
+    if (input_usuario == usuario && input_contrasena == contrasena) {
+        cout << "Iniciaste sesion" ;
+    } else {
+        cout << "Usuario o contraseña incorrectos. Reintentalo";
+    }
+
+    return 0;
+}

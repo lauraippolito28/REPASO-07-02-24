c2dac605f44dcf96458b6bfa49e516ddd9208890
lauraippolito28 <159057618+lauraippolito28@users.noreply.github.com>
2024-02-06 19:53:57 -0500

    Update cara_y_sello.cpp

--- cara_y_sello.cpp
+++ cara_y_sello.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>  
 
+using namespace std;
+
+int main() {
+    // Entrada y variables
+    srand(time(0));
+
+    // Lanzamiento 
+    int resultado_moneda = rand() % 2;
+
+    // Opcion del jugador 
+    int opcion_jugador;
+    cout << "Elige cara o sello:\n ";
+    cin >> opcion_jugador;
+
+    // Mostrar el resultado del lanzamiento
+    cout << "Resultado del lanzamiento: ";
+    if (resultado_moneda == opcion_jugador)
+        cout << "Ganaste";
+    else
+        cout << "Perdiste";
+
+    return 0;
+}

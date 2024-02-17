09759f68118163f22f26f778308ca76f0f24321d
lauraippolito28 <159057618+lauraippolito28@users.noreply.github.com>
2024-02-16 20:55:40 -0500

    Update punto7.cpp

---
+++ TALLER_0/punto7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // Entradas del usuario
+    cout << "Ingresa un numero ";
+    cin >> n;
+
+    // Proceso y salida 
+    if (numero % 2 == 0) {
+        cout << "El numero es par." << endl;
+    } else {
+        cout << "El numero es impar" << endl;
+    }
+
+    return 0;
+}
